b23534dfaaba713d8a752309d756168546cc0bb3
rr561 <ronron_21@yahoo.com>
2023-12-02 22:44:24 +0200

    refactor(frontend): switched to no standalone

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,23 +1,14 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { TestObject, TestService } from './services/test-service';
 import { Observable } from 'rxjs';
-import { HttpClientModule } from '@angular/common/http';
+import { TestObject, TestService } from './test/test-service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet,HttpClientModule],
-  providers: [TestService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+ message: Observable<TestObject> = this.testService.getMessage()
 
-  message: Observable<TestObject> = this.testService.getMessage()
-  constructor(private testService: TestService) {
-
-  }
-
+  constructor(private testService: TestService) {}
 }
